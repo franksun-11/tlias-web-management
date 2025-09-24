434119560bc218f6f56d61daecc9a6b28871b4ad
sunwe <s2564099@ed.ac.uk>
2025-09-24 17:41:06 +0100

    运用阿里云OSS实现文件上传

--- src/main/java/org/example/controller/uploadController.java
+++ src/main/java/org/example/controller/uploadController.java
@@ -2,6 +2,8 @@ package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.pojo.Result;
+import org.example.utils.AliyunOSSOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +15,10 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class uploadController {
-    @PostMapping("/upload")
+    /**
+     * 本地磁盘存储方案
+     */
+/*    @PostMapping("/upload")
     public Result upload(String name, Integer age, MultipartFile file) throws IOException {
         log.info("接收参数: {}, {}, {}", name, age, file);
         // 获取原始文件名
@@ -24,5 +29,15 @@ public class uploadController {
         // 保存文件
         file.transferTo(new File("D:/images/" + newFileName));
         return Result.success();
+    }*/
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("文件上传: {}", file.getOriginalFilename());
+        // 将文件交给阿里云oss进行存储
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        log.info("文件上传OSS, url: {}", url);
+        return Result.success(url);
     }
 }
--- src/main/java/org/example/service/impl/EmpServiceImpl.java
+++ src/main/java/org/example/service/impl/EmpServiceImpl.java
@@ -72,7 +72,6 @@ public class EmpServiceImpl implements EmpService {
             emp.setCreateTime(LocalDateTime.now());
             emp.setUpdateTime(LocalDateTime.now());
             empMapper.insert(emp);
-            int i = 1 / 0;
             // 2.保存员工工作经历信息
             List<EmpExpr> exprList = emp.getExprList();
             if(!CollectionUtils.isEmpty(exprList)){
