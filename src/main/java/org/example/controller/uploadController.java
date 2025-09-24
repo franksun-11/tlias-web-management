d76b92df8f6a849c1539da37773e0fa9f5ea5ed5
sunwe <s2564099@ed.ac.uk>
2025-09-24 14:49:57 +0100

    测试文件上传功能(upload.html, uploadController)

---
+++ src/main/java/org/example/controller/uploadController.java
@@ -0,0 +1,17 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.pojo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+public class uploadController {
+    @PostMapping("/upload")
+    public Result upload(String name, Integer age, MultipartFile file){
+        log.info("接收参数: {}, {}, {}", name, age, file);
+        return Result.success();
+    }
+}
