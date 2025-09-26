281f2862586de93c06c4a372741cba9158f2c03c
sunwe <s2564099@ed.ac.uk>
2025-09-26 15:02:16 +0100

    实现员工分页查询

---
+++ src/main/java/org/example/controller/ClassController.java
@@ -0,0 +1,32 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.pojo.ClassQueryParam;
+import org.example.pojo.Clazz;
+import org.example.pojo.PageResult;
+import org.example.pojo.Result;
+import org.example.service.ClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/clazzs")
+public class ClassController {
+    @Autowired
+    private ClassService classService;
+
+    /**
+     * 分页查询班级
+     */
+    @GetMapping
+    public Result page(ClassQueryParam classQueryParam){
+        log.info("分页查询班级数据:{},{},{},{},{}" + classQueryParam);
+        PageResult<Clazz> pageResult =classService.page(classQueryParam);
+        return Result.success(pageResult);
+    }
+}
